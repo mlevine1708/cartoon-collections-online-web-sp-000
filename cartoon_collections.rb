db24874b5d7da03536fb402afc37e3516ad29858
Micah Levine <micah.levine@outlook.com>
2019-08-23 18:57:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,15 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+long_planeteer_calls = "puff", "go", "two", "industrious", "bop"
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i = i + 1
+  end
 end
 
 def find_the_cheese# code an argument here
